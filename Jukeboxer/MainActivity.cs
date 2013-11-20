79c371951dc7136ea3747f2e092dd664b7568ac2
Isaac Datlof <ike18t@gmail.com>
2013-11-19 23:06:54 -0500

    Updated project/solution name and paths

--- Jukeboxer/MainActivity.cs
+++ Jukeboxer/MainActivity.cs
@@ -7,45 +7,39 @@ using Android.Widget;
 using Android.OS;
 using Android.Bluetooth;
 
-namespace AndroidXamarin
+namespace Jukeboxer
 {
-	[Activity (Label = "AndroidXamarin", MainLauncher = true)]
+	[Activity(Label = "Jukeboxer", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
-		protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate (bundle);
+			base.OnCreate(bundle);
 
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			SetContentView(Resource.Layout.Main);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
 			
 			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+				BluetoothAdapter.DefaultAdapter.StartDiscovery();
 			};
 
 			var receiver = new BluetoothReceiver();
 			var bluetoothFilter = new IntentFilter(BluetoothDevice.ActionFound);
 			RegisterReceiver(receiver, bluetoothFilter);
-
-			var bluetooth = BluetoothAdapter.DefaultAdapter;
-			bluetooth.StartDiscovery ();
 		}
 	}
 
 	public class BluetoothReceiver : BroadcastReceiver
 	{
-		public void OnReceive(Context context, Intent intent)
+		public override void OnReceive(Context context, Intent intent)
 		{
 			if(BluetoothDevice.ActionFound == intent.Action)
 			{
 				var device = (BluetoothDevice)intent.GetParcelableExtra(BluetoothDevice.ExtraDevice);
-				var rssi = intent.GetShortExtra(BluetoothDevice.ExtraRssi, 0);
 				Toast.MakeText(context, device.Name, ToastLength.Long).Show();
 			}
 		}
